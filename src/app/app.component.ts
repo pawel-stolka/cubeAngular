043530868ae43da332a8f6885d27527726a905e7
CUBEITG\pstolka <pstolka@cubeitg.pl>
2017-10-20 10:56:16 +0200

    transaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'placki';
+
+counter = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.counter++;
+
+    }, 500);
+  }
+
+  data = {
+    message: 'jestem cool'
+  };
 }
