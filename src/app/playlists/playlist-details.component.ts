25e9daed769708811d149ba5910990e1ae88881d
CUBEITG\pstolka <pstolka@cubeitg.pl>
2017-10-20 15:20:39 +0200

    obserwator JS

--- src/app/playlists/playlist-details.component.ts
+++ src/app/playlists/playlist-details.component.ts
@@ -28,6 +28,25 @@ interface Playlist {
       </div>
   </div>
 </div>
+<div>
+  <div class="form-group">
+    <label>Name</label>
+    <input class="form-control"
+        (keyup)=" playlist.name = $event.target.value "
+        [value]="playlist.name">
+  </div>
+  <div class="form-group">
+    <label>Favourite</label>
+    <input type="checkbox" class="form-control"
+      (change)=" playlist.favourite = $event.target.checked "
+      [checked]="playlist.favourite">
+  </div>
+  <div class="form-group">
+    <label>Color</label>
+    <input type="color" [value]="playlist.color"
+      (change)=" playlist.color = $event.target.value"
+      [value]="playlist.color">
+</div>
 `, styles: []
 })
 export class PlaylistDetailsComponent implements OnInit {
